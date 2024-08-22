de8cb065d30663bf37682cc17a18cafee1f87044
joao vitor <joaogianoni.tds2.senai@gmail.com>
2024-08-22 17:16:12 -0300

    finalization

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import { config } from 'dotenv'
+import express from 'express'
+
+config()
+
+const  serverPort = process.env.PORT || 4000
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res .status(200).send({ message: "Hello, World!"})
+})
+
+app.listen(serverPort, () => {
+    console.log(`🤑 Server started on http://localhost:${serverPort}`)
+})

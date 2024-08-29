538d0c601da8b72db8a145180f643f1bc1040762
joao vitor <joaogianoni.tds2.senai@gmail.com>
2024-08-29 11:49:13 -0300

    feat: start to refactor api routes

---
+++ src/routes/emocoes.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express"
+
+const emocoesRoutes = Router()
+
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Tristeza",
+        cor: "Azul"
+    },
+]
+
+app.get("/emocoes", (req, res) => {
+    return res .status(200)
+    .send( emocoes )
+})
+app.post("/emocoes", (req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+
+    emocoes.push(novaEmocao)
+    return res .status(200)
+    .send(emocoes)
+})

55c7630a2830ea5e1bcc11e9169e87703e753d53
joao vitor <joaogianoni.tds2.senai@gmail.com>
2024-08-29 10:53:24 -0300

    feat: finalization

--- src/server.js
+++ src/server.js
@@ -9,11 +9,55 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    return res .status(200).send({ message: "Hello, World!"})
-})
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Tristeza",
+        cor: "Azul"
+    },
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Batman",
+        studio: "DC comics",
+        vivo: true
+    },
+    {
+        id: 101,
+        nome: "Deadpool",
+        studio: Disney,
+        vivo: true
+    },
+    {
+        id: 102,
+        nome: "Kevin Garvey",
+        studio: "HBO",
+        vivo: true
+    },
+]
 app.get("/2tds2", (req, res) => {
-    return res .status(200).send({ message: "Hello, World!"})
+    return res .status(200)
+    .send( emocoes )
+})
+app.get("/emocoes", (req, res) => {
+    return res .status(200)
+    .send( emocoes )
+})
+app.get("/personagens", (req, res) => {
+    return res .status(200)
+    .send( personagens )
 })
 
 app.listen(serverPort, () => {

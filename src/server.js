e3f14ccf3e7ce78656ae8c89dcadef0e49fcbb2e
joao vitor <joaogianoni.tds2.senai@gmail.com>
2024-08-29 09:13:44 -0300

    feat: finish server config

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ app.use(express.json())
 app.get("/", (req, res) => {
     return res .status(200).send({ message: "Hello, World!"})
 })
+app.get("/2tds2", (req, res) => {
+    return res .status(200).send({ message: "Hello, World!"})
+})
 
 app.listen(serverPort, () => {
     console.log(`🤑 Server started on http://localhost:${serverPort}`)
